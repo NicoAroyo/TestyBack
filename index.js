8168bea93b121153f5b36afe699ae1f32b919443
NicoAroyo <nico.aroyo@gmail.com>
2022-08-24 20:35:35 +0300

    afh

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+
 import express from "express";
 import bodyParser from "body-parser";
 import questionsRoutes from "./routes/questions.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const  db = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/test', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      
+    })
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+
+
 
 const app = express();
 const PORT = 5000;
