163c8833d04a3eac1e8b0cc0d3b6141b350eff7b
NicoAroyo <nico.aroyo@gmail.com>
2022-08-29 16:12:12 +0300

    cannot sign up with same email

--- routes/auth.js
+++ routes/auth.js
@@ -26,4 +26,25 @@ authRouter.post("/", async (req, res) => {
   }
 });
 
+authRouter.post("/sign-up", async (req, res) => {
+  const credentials = req.body;
+  try {
+    const user = await UserModel.findOne({
+      email: credentials.email,
+    }).exec();
+    console.log(user);
+
+    if (!user) {
+      res.status(500).send(Error("Invalid credentials"));
+    } else {
+      res.send({
+        token: "token123",
+        user,
+      });
+    }
+  } catch (error) {
+    res.send(500).json({ message: error.message });
+  }
+});
+
 export default authRouter;
