9df536af72d1ca325d6d0bfdc8cb9688307da324
NicoAroyo <nico.aroyo@gmail.com>
2022-08-30 11:31:54 +0300

    reportservice

--- model/report.js
+++ model/report.js
@@ -2,18 +2,23 @@ import mongoose from "mongoose";
 import user from "./user";
 
 const reportScheme = new mongoose.Schema({
-  grade: {
-    required: true,
-    type: Number,
-  },
-  student: {
-    required: true,
-    type: user,
-  },
-  quizId: {
-    required: true,
-    type: String,
-  },
+    
+    grade :
+    {
+        required: true,
+        type: Number
+    },
+    student: 
+    {
+        required: true,
+        type: user
+    },
+    quizId : {
+
+        required: true,
+        type: String
+    }
+
 });
 
-export default mongoose.model("Report", quizScheme);
+export default mongoose.model("Report", reportScheme);
